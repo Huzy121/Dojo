05119bc8a0b0d266cf77469dba7a2dbb33c25606
Huzy121 <hmamaniyat@gmail.com>
2024-08-24 20:47:15 +0100

    Added a scrollable view for all audios

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -2,92 +2,102 @@
 
 import 'package:dojo/Widgets/custom_navigation_bar.dart';
 import 'package:dojo/assets/constants.dart';
+import 'package:dojo/assets/riverpod.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart' as path;
 
-class HomePage extends StatefulWidget {
+class HomePage extends ConsumerWidget {
   @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final recordingList = ref.watch(recordingListFutureProvider);
     return Scaffold(
       body: Column(
         children: [
-          Expanded(
-            child: Center(
-              child: Container(
-                constraints: BoxConstraints.expand(),
-                child: SafeArea(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: 10.0, left: 10.0, right: 10.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Good Morning,',
-                                  style: kWelcomeText,
-                                ),
-                                Text(
-                                  'Huzaifah',
-                                  style: kWelcomeName,
-                                ),
-                              ],
-                            ),
-                            Image.asset(
-                              'images/man.png',
-                              width: 60.0,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            left: 50, right: 50.0, top: 25.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Expanded(
-                              child: GestureDetector(
-                                child: Container(
-                                  padding: EdgeInsets.only(
-                                    left: 20.0,
-                                    top: 20.0,
-                                  ),
-                                  child: Text(
-                                    'Interstitial Lung Disease',
-                                    style: kRecordingTitle,
-                                  ),
-                                  height: 400.0,
-                                  width: 50.0,
-                                  decoration: BoxDecoration(
-                                    color: Colors.blue,
-                                    borderRadius: BorderRadius.circular(
-                                        20), // Adjust the radius here
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Good Morning,',
+                      style: kWelcomeText,
+                    ),
+                    Text(
+                      'Huzaifah',
+                      style: kWelcomeName,
+                    ),
+                  ],
                 ),
-              ),
+                Image.asset(
+                  'images/man.png',
+                  width: 60.0,
+                ),
+              ],
             ),
           ),
-          CustomNavigationBar(), // Custom navigation bar at the bottom
+          Expanded(
+            child: recordingList.when(
+              data: (files) {
+                return PageView.builder(
+                  scrollDirection: Axis.horizontal, // Horizontal scrolling
+                  itemCount: files.length,
+                  itemBuilder: (context, index) {
+                    final file = files[index];
+                    final fileName =
+                        path.basename(file.path).replaceAll('.m4a', '');
+                    return RecordingTile(title: fileName);
+                  },
+                );
+              },
+              loading: () => Center(
+                child: CircularProgressIndicator(),
+              ),
+              error: (error, stack) => Center(child: Text('Error: $error')),
+            ),
+          ),
+          Spacer(), // Add spacer to fill remaining space and push the nav bar to the bottom
         ],
-      ), 
+      ),
+      bottomNavigationBar:
+          CustomNavigationBar(), // Fix navigation bar at the bottom
+    );
+  }
+}
+
+class RecordingTile extends StatelessWidget {
+  final String title;
+  const RecordingTile({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('Tapped on: $title');
+      },
+      child: Container(
+        height: 700.0, // Fixed height for each tile
+        width: double.infinity, // Takes full width of the screen
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          title,
+          style: kRecordingTitle,
+        ),
+      ),
     );
   }
 }
--- lib/assets/riverpod.dart
+++ lib/assets/riverpod.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final recordingTitleRiverpod = StateProvider<String>((ref) {
@@ -5,4 +7,28 @@ final recordingTitleRiverpod = StateProvider<String>((ref) {
 });
 final isRecordingProvider = StateProvider<bool>((ref) {
   return false;
+});
+final recordingListFutureProvider =
+    FutureProvider<List<FileSystemEntity>>((ref) async {
+  Directory? directory;
+
+  if (Platform.isAndroid) {
+    // Use getExternalStorageDirectory for Android
+    directory = await getExternalStorageDirectory();
+  } else if (Platform.isIOS) {
+    // Use getApplicationDocumentsDirectory for iOS
+    directory = await getApplicationDocumentsDirectory();
+  }
+
+  // If directory is null, return an empty list
+  if (directory == null) {
+    print('Directory is null');
+    return [];
+  }
+
+  // List all files in the directory
+  final List<FileSystemEntity> files = directory.listSync();
+  print(files.toString());
+
+  return files;
 });
